a38dbf121c8a8cf570cbccdb81f9b8f5d5874ff5
lordstarkk <166766299+lordstarkk@users.noreply.github.com>
2024-07-09 21:07:10 +0530

    Inital commit

--- MathsProblem.py
+++ MathsProblem.py
@@ -9,7 +9,7 @@ easyOperators = ["+", "-"]
 
 wrong = 0
 
-def problemGenrator():
+def hardProblemGenrator():
     left = random.randint(min_operand, max_operand)
     right = random.randint(min_operand, max_operand)
     oprator = random.choice(hardOprators)
@@ -47,35 +47,100 @@ def mediumProblemGenrator():
 
 
 while True:
+    difficulty_level = input("What Level you want(Easy, Meduim or Hard)?").lower()
     number_of_ques = int(input("How many questions you want? "))
-    q = input("Ready? (y/n?)")
-    startTime = time.time()
-    if q == "y":
-        for i in range(number_of_ques):
-            expr, answer = problemGenrator()
-            while True:
-                guess = input(f"#{i+1}. {expr} = ")
-                if guess == str(answer):
-                    print("Correct Answer!\n")
-                    break
-                elif guess == "s":
-                    expr, answer = problemGenrator()
-                elif guess != str(answer):
-                    print("Wrong Answer")
-                    print(f"Corrent Answer: {answer}\n")
-                    wrong=+1
-                    expr, answer = problemGenrator()
-                    break
-        endTIme = time.time()
-        final = endTIme-startTime
-        correct = 10-wrong
-        print(f"Correct Answers: {int(correct)}")
-        print(f"You did it in {int(final)} Seconds")
-        o = input("\nWanna play again? ")
-        if o == "y":
+    if difficulty_level == "hard":
+        q = input("Ready? (y/n?)")
+        startTime = time.time()
+        if q == "y":
+            for i in range(number_of_ques):
+                expr, answer = problemGenrator()
+                while True:
+                    guess = input(f"#{i+1}. {expr} = ")
+                    if guess == str(answer):
+                        print("Correct Answer!\n")
+                        break
+                    elif guess == "s":
+                        expr, answer = problemGenrator()
+                    elif guess != str(answer):
+                        print("Wrong Answer")
+                        print(f"Corrent Answer: {answer}\n")
+                        wrong=+1
+                        expr, answer = problemGenrator()
+                        break
+            endTIme = time.time()
+            final = endTIme-startTime
+            correct = 10-wrong
+            print(f"Correct Answers: {int(correct)}")
+            print(f"You did it in {int(final)} Seconds")
+            o = input("\nWanna play again? ")
+            if o == "y":
+                break
+            else:
+                exit()
+        elif q=="n":
             break
-        else:
-            exit()
-    elif q=="n":
-        break
+    
+    elif difficulty_level == 'medium':
+        q = input("Ready? (y/n?)")
+        startTime = time.time()
+        if q == "y":
+            for i in range(number_of_ques):
+                expr, answer = mediumProblemGenrator()
+                while True:
+                    guess = input(f"#{i+1}. {expr} = ")
+                    if guess == str(answer):
+                        print("Correct Answer!\n")
+                        break
+                    elif guess == "s":
+                        expr, answer = mediumProblemGenrator()
+                    elif guess != str(answer):
+                        print("Wrong Answer")
+                        print(f"Corrent Answer: {answer}\n")
+                        wrong=+1
+                        expr, answer = mediumProblemGenrator()
+                        break
+            endTIme = time.time()
+            final = endTIme-startTime
+            correct = 10-wrong
+            print(f"Correct Answers: {int(correct)}")
+            print(f"You did it in {int(final)} Seconds")
+            o = input("\nWanna play again? ")
+            if o == "y":
+                break
+            else:
+                exit()
+        elif q=="n":
+            break
 
+    elif difficulty_level == "easy":
+        q = input("Ready? (y/n?)")
+        startTime = time.time()
+        if q == "y":
+            for i in range(number_of_ques):
+                expr, answer = easyProblemGenrator()
+                while True:
+                    guess = input(f"#{i+1}. {expr} = ")
+                    if guess == str(answer):
+                        print("Correct Answer!\n")
+                        break
+                    elif guess == "s":
+                        expr, answer = easyProblemGenrator()
+                    elif guess != str(answer):
+                        print("Wrong Answer")
+                        print(f"Corrent Answer: {answer}\n")
+                        wrong=+1
+                        expr, answer = easyProblemGenrator()
+                        break
+            endTIme = time.time()
+            final = endTIme-startTime
+            correct = 10-wrong
+            print(f"Correct Answers: {int(correct)}")
+            print(f"You did it in {int(final)} Seconds")
+            o = input("\nWanna play again? ")
+            if o == "y":
+                break
+            else:
+                exit()
+        elif q=="n":
+            break
