0658700d35cd508c14823b6a127a7f0a421f7eb3
lordstarkk <166766299+lordstarkk@users.noreply.github.com>
2024-12-23 00:59:25 +0530

    Changed whole file

--- Text-to-speech.py
+++ Text-to-speech.py
@@ -1,46 +1,20 @@
 #A beginner project
-#This simple program converts the text into speech
-#created by stark
+#This simple program converts the text into speech using pyttsx3 module
 
+# importing module
+import pyttsx3
 
-import win32com.client
+# defining engine
+engine = pyttsx3.init()
 
-v = win32com.client.Dispatch("SAPI.SpVoice")
+# getting voices and speech rate
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
 
+# setting voice and speech rate
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 160)
 
-
-print('Press "q" to Quit')
-name = input("May i know your name? ")
-
-
-
-if name == "Stark" or "nirbhay":
-    print("Welcome Sir!")
-    v.Speak("Welcome Sir")
-else:
-    print(f"Hello! {name}")
-    v.Speak(f"Hello!{name}")
-    print("I am a text to speech program created by Stark \nWhat do you want me to speak?")
-    v.Speak("I am a text to speech program created by Stark. What do you want me to speak?")
-
-while True:
-    text = input("Enter a Text:")
-     
-    if text == "q":
-            v.Speak("Bye bye")
-            print("Bye! bye!")
-            break
-
-    if text == "who created you":
-        print("Created by Stark")
-        v.Speak("I am a Text to speech program Created by mister Stark")
-        
-        continue
-
-    if text == "whats your instagram":
-        print(contact)
-        v.Speak(contact)
-        continue
-
-
-    v.Speak(text)
+# defining speak function
+def speak(text):
+    pyttsx3.speak(text)
